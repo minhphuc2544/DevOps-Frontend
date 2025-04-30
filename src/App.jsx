27c253e237896ca7a81518678efe5f92711fad8e
minhphuc2544 <phucle190904@gmail.com>
2025-04-30 21:09:53 +0700

    fix(refactor): display menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Menu from "./Menu";
+
 export default function App() {
   const baseURL = import.meta.env.VITE_BASE_URL || '/';
   return (
+    <div className="app-container">
+       <Menu />
     <Routes>
       <Route path={`${baseURL}*`} element={<Navigate to={baseURL} />} />
       <Route path={`${baseURL}/`} element={<Home />} />
     </Routes>
+    </div>
   )
 }
 
